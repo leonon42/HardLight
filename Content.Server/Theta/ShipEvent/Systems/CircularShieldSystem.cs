56ef244153a4ec7dfe42a913e8c9e86f06c972af
Ark <189933909+ark1368@users.noreply.github.com>
2025-05-17 16:12:14 -0700

    Shield Crash Fix (#497)

--- Content.Server/Theta/ShipEvent/Systems/CircularShieldSystem.cs
+++ Content.Server/Theta/ShipEvent/Systems/CircularShieldSystem.cs
@@ -620,7 +620,7 @@ public sealed class CircularShieldSystem : SharedCircularShieldSystem
             return false;
 
         UpdateConsoleState(console);
-        Dirty(console, shield.Comp);
+        Dirty(shield.Owner, shield.Comp);
 
         return true;
     }
